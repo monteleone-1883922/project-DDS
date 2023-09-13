cb85abf83c1ad5c4b021c99f3b9e0493f693096a
AleG19 <monteleone.1883922@studenti.uniroma1.it>
2023-09-13 21:54:20 +0200

    errors in simulation fixed

---
+++ garay_algorithm/out/clang-debug/infected_m.o.d
@@ -0,0 +1,6 @@
+out/clang-debug//infected_m.o: infected_m.cc infected_m.h \
+  /home/alex/Programs/omnetpp-6.0.1/include/omnetpp.h
+
+infected_m.h:
+
+/home/alex/Programs/omnetpp-6.0.1/include/omnetpp.h:
---
+++ garay_algorithm/out/clang-debug/process.o.d
@@ -0,0 +1,15 @@
+out/clang-debug//process.o: process.cc process.h \
+  /home/alex/Programs/omnetpp-6.0.1/include/omnetpp.h sendValue_m.h \
+  kingSend_m.h sendList_m.h infected_m.h
+
+process.h:
+
+/home/alex/Programs/omnetpp-6.0.1/include/omnetpp.h:
+
+sendValue_m.h:
+
+kingSend_m.h:
+
+sendList_m.h:
+
+infected_m.h:
