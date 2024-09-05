cf353f98100416c4c1248bcef72236ea1537ad80
alex <monteleone.1883922@studenti.uniroma1.it>
2024-09-05 17:48:01 +0200

    fixed unordered visualization

--- garay_algorithm/scripts/visualize_experiments_results.py
+++ garay_algorithm/scripts/visualize_experiments_results.py
@@ -2,8 +2,23 @@ import sys
 import os
 import json
 import matplotlib.pyplot as plt
+import re
 
 EXPERIMENTS_DIR = 'results/experiments'
+
+
+def estrai_numero(file_name, titolo):
+    # Crea un pattern per trovare il titolo e il numero associato
+    pattern = rf"{titolo}-(\d+)"
+
+    # Cerca il pattern nel nome del file
+    match = re.search(pattern, file_name)
+
+    if match:
+        # Restituisce il numero trovato
+        return match.group(1)
+    else:
+        return None
 
 
 def visualize_results(visualization):
@@ -25,6 +40,7 @@ def visualize_results(visualization):
         num_processes = visualization['numProcesses'] if 'numProcesses' in visualization else None
         infection_speed = visualization['infectionSpeed'] if 'infectionSpeed' in visualization else None
         x_label = 'numInfected' if num_infected is None else 'numProcesses' if num_processes is None else 'infectionSpeed'
+        title = 'infected' if num_infected is None else 'numProc' if num_processes is None else 'speed'
         fields_data = {}
 
         # Collect data from all JSON files in the experiments directory
@@ -33,21 +49,27 @@ def visualize_results(visualization):
                     and (num_infected is None or f'infected-{num_infected}' in file) \
                     and (num_processes is None or f'numProc-{num_processes}' in file) \
                     and (infection_speed is None or f'speed-{infection_speed}' in file):
+                num_experiment = int(estrai_numero(file, title))
                 with open(os.path.join(EXPERIMENTS_DIR, file), 'r') as f:
                     results = json.load(f)
                 for key, value in results.items():
                     if value is not None:
-                        fields_data[key] = fields_data.get(key, []) + [value]
-        for field, data in fields_data.items():
-            plt.figure(figsize=(10, 6))
+                        fields_data[key] = fields_data.get(key, []) + [(num_experiment, value)]
 
-            plt.plot(data, label=field)
-            plt.xlabel(x_label)
-            plt.ylabel(field.replace('_', ' ').title())
-            plt.title(f'{field.replace("_", " ").title()} per Experiment')
-            plt.legend()
-            plt.grid(True)
-            plt.show()
+        for field, data in fields_data.items():
+            if field != 'rounds_means_times':
+                sorted_data = sorted(data, key=lambda x: x[0])
+                plottable_list = [val for _, val in sorted_data]
+                x_values = [val for val, _ in sorted_data]
+                plt.figure(figsize=(10, 6))
+
+                plt.plot(x_values, plottable_list, label=field)
+                plt.xlabel(x_label)
+                plt.ylabel(field.replace('_', ' ').title())
+                plt.title(f'Experiment on {field.replace("_", " ").title()}')
+                plt.legend()
+                plt.grid(True)
+                plt.show()
 
 
 def main():
